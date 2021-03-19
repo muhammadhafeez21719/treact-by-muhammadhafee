2faf682a877dd50a06e2941d8abd2d165dd20074
muhammadhafeez1719 <muhammadhafeez1719@yahoo.com>
2021-03-19 20:55:09 +0500

    treact  by muhammad hafeez

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCUO708naSY04CaMXLpGuu_WNS_AL44B4o",
+    authDomain: "treact-by-muhammadhafeez.firebaseapp.com",
+    projectId: "treact-by-muhammadhafeez",
+    storageBucket: "treact-by-muhammadhafeez.appspot.com",
+    messagingSenderId: "332179322546",
+    appId: "1:332179322546:web:4e7c875d5a3c5c1ee93bab",
+    measurementId: "G-1T6EBS9DDL"
+  };
